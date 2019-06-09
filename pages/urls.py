6dab0cdab2d01c33e659ad42ce70e701ea562599
Jhoel <jhoel.jjpr@gmail.com>
2019-06-09 18:58:26 -0500

    about.html change template

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import Home
+from .views import Home, AboutPageView
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('about/',AboutPageView.as_view(),name = 'about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
