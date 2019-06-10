2210a80876978d22a645898a7e945f671731d11e
Jhoel <jhoel.jjpr@gmail.com>
2019-06-09 19:24:15 -0500

    added test and base, cannot run test succesfully

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+from django.tests import SimpleTestCase
+
+class PageTests(SimpleTestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_aboutpage_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
